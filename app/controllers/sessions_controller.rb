a2f79fabd329a78b18ee4c083e8e790c97b441ea
sujunhao <spike76@qq.com>
2018-11-26 09:47:46 +0800

    sign页面使用单独的layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,17 @@
 class SessionsController < ApplicationController
 
+  layout 'login'
+
   def create
     admin = Admin.find_by(name: params[:name])
     if admin && admin.authenticated?(params[:password])
       session[:admin_id] = admin.id
       flash.now[:success] = 'success'
-      redirect_to welcome_path
+      redirect_to new_post_path
     else
       flash.now[:danger] = 'Failed: name and password do not match'
       render :new
     end
-  end
-
-  def welcome
-    p 1
   end
 
 end
