cbb2bbc7ca728ac9f54e017c107836355a754cd9
sujunhao <spike76@qq.com>
2018-11-27 17:55:28 +0800

    解决markdown中代码块显示问题

--- config/initializers/markdown_renderer.rb
+++ config/initializers/markdown_renderer.rb
@@ -1,2 +1,11 @@
 # Initializes a Markdown parser
-MkRenderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+extensions = {
+  no_intra_emphasis: true,
+  fenced_code_blocks: true,
+  autolink: true,
+  strikethrough: true,
+  space_after_headers: true,
+  superscript: true
+}
+
+MkRenderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions)
