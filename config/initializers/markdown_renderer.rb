781633f88a443d4dbb99033979e3fd25bb13ee57
sujunhao <spike76@qq.com>
2018-11-29 15:06:26 +0800

    bug-fix: markdown codeblock显示

--- config/initializers/markdown_renderer.rb
+++ config/initializers/markdown_renderer.rb
@@ -1,11 +1,36 @@
 # Initializes a Markdown parser
+
+render_options = {
+  # will remove from the output HTML tags inputted by user
+  filter_html: true,
+  # will insert <br /> tags in paragraphs where are newlines
+  hard_wrap: true,
+  # hash for extra link options, for example 'nofollow'
+  link_attributes: { rel: 'nofollow' },
+  # Prettify
+  prettify: true
+}
+renderer = Redcarpet::Render::HTML.new(render_options)
+
 extensions = {
-  no_intra_emphasis: true,
-  fenced_code_blocks: true,
+  #will parse links without need of enclosing them
   autolink: true,
+  # blocks delimited with 3 ` or ~ will be considered as code block.
+  # No need to indent.  You can provide language name too.
+  # ```ruby
+  # block of code
+  # ```
+  fenced_code_blocks: true,
+  # will ignore standard require for empty lines surrounding HTML blocks
+  lax_spacing: true,
+  # will not generate emphasis inside of words, for example no_emph_no
+  no_intra_emphasis: true,
+  # will parse strikethrough from ~~, for example: ~~bad~~
   strikethrough: true,
-  space_after_headers: true,
+  # will parse superscript after ^, you can wrap superscript in ()
   superscript: true
+  # will require a space after # in defining headers
+  # space_after_headers: true
 }
 
-MkRenderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions)
+MkRenderer = Redcarpet::Markdown.new(renderer, extensions)
